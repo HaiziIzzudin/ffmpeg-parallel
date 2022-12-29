d754d35f872bafc37df8050b4e9fbcf595ddf38f
Nama diberi Haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2022-12-29 20:47:07 +0800

    Add files via upload

--- 3worker.py
+++ 3worker.py
@@ -1,12 +1,7 @@
-import subprocess
-import json
+import subprocess, json, datetime, os
 from os import system, name
-import datetime
-import os
-import ffmpeg
 
-
-# FUNCTIONS
+# FUNCTIONS TO CLEAR TERMINAL
 def clear():
     if name == 'nt': # for windows
         _ = system('cls')
@@ -15,22 +10,14 @@ def clear():
 
 clear();
 
-
-
 # get file input
 fpth = input("Drag video file into this program: ")
 
-
-
 # extract and print time in seconds (we use ffprobe here)
-input_filename = "fpth"
 out = subprocess.check_output(["ffprobe", "-v", "quiet", "-show_format", "-print_format", "json", fpth])
 ffprobe_data = json.loads(out)
 dur = float(ffprobe_data["format"]["duration"])
-print(dur)
-
-
-
+print("Total duration in seconds is "+ str(dur))
 
 # now, find its HH:MM:SS for each worker
 w1ss = str(datetime.timedelta(seconds = (0/3)*dur))
@@ -43,13 +30,7 @@ workerSSArr = [w1ss,w2ss,w3ss]
 workerTTArr = [w1tt,w2tt,w3tt]
 print(workerSSArr,workerTTArr)
 
-
-
-
 # invoke new ffmpeg process with its own ffmpeg commands
-
-
-
 
 ## but before that, we need to configure outputName
 fpthOut1 = (fpth[:-4] + "_AV1-1.mp4")
@@ -58,21 +39,13 @@ fpthOut3 = (fpth[:-4] + "_AV1-3.mp4")
 print("\n\nProcessed video will be named as follows:\n" + fpthOut1 + "\n" + fpthOut2 + "\n" + fpthOut3)
 fpthOutArr = [fpthOut1,fpthOut2,fpthOut3]
 
-
-
-
 ## now we can invoke command ffmpeg (in new terminal possibly)
 
-
-
-
 ### filename has to change according to operating system
-workerFilenameNTKnl = ["worker1.ps1","worker2.ps1","worker3.ps1"]
-workerFilenamePosix = ["worker1.sh","worker2.sh","worker3.sh"]
-  
+workerFilenameNTKnl = ["worker1.bat","worker2.bat","worker3.bat"]
+#workerFilenamePosix = ["worker1.sh","worker2.sh","worker3.sh"]
+workerFilenamePy = ["worker1.py","worker2.py","worker3.py"]
 ffmpegCMD = " -c:v libsvtav1 -preset 7 -b:v 2.5k -c:a aac -b:a 192k -t "
-
-
 
 # condition to create worker source file
 z = -1
@@ -81,8 +54,10 @@ if name == 'nt': # for windows
         z += 1
         f = open(x, "a")
         f.write("ffmpeg -ss "+workerSSArr[z]+" -i "+ fpth + ffmpegCMD + workerTTArr[z] +" "+ fpthOutArr[z])
+        
+        
 else: # for mac and linux(here, os.name is 'posix')
-    for x in workerFilenamePosix:
+    for x in workerFilenamePy:
         z += 1
         f = open(x, "a")
         f.write("ffmpeg -ss "+workerSSArr[z]+" -i "+ fpth + ffmpegCMD + workerTTArr[z] +" "+ fpthOutArr[z])
