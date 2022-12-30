72a540659644ea1c5dc76ccbe89a738d46cae80f
Nama diberi Haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2022-12-30 12:19:09 +0800

    asas

--- 3worker.py
+++ 3worker.py
@@ -64,3 +64,23 @@ fpthOut3 = (videoPath[:-4] + "_AV1-3.mp4")
 print("\nProcessed video will be named as following:\n" + fpthOut1 + "\n" + fpthOut2 + "\n" + fpthOut3)
 fpthOutArr = [fpthOut1,fpthOut2,fpthOut3]
 
+
+## Command template
+a = "ffmpeg -ss "
+b = "-i "
+c = "-c:v libsvtav1 -b:v 2.5M -preset 7 -c:a aac -b:a 192k -t "
+d = " "
+
+
+
+## create temporary worker file
+
+if name == 'nt': # for windows
+        wFile = ["w1.bat","w2.bat","w3.bat"]
+else:            # for mac and linux
+        wFile = ["w1.sh","w2.sh","w3.sh"]
+
+for x in wFile:
+    f = open( x , "a")
+    f.write(a + w1ss + b + videoPath + c + w2sT + d + fpthOut1)
+    f.close()
